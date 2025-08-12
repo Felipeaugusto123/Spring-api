314cef6f4c78695638ceeba000a131a67c39b292
felipeaugusto123 <felipeaugustorodriguesmartins@gmail.com>
2025-08-12 18:26:20 -0300

    Add handler exception in user

---
+++ course/src/main/java/com/felipeAugusto/course/servicies/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.felipeAugusto.course.servicies.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found: Id " + id);
+    }
+
+}
